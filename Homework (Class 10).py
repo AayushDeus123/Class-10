89e772b95d29c7ea8d29d5f0a36851acfa579e3d
AayushDeus123 <aayushdeus312@gmail.com>
2024-12-30 20:34:04 +0530

    Add files via upload

---
+++ Homework (Class 10).py
@@ -0,0 +1,5 @@
+#Power calculator
+base = int(input('Enter a number : '))
+power = int(input('Enter the power : '))
+ans = base**power
+print('The number ',base, 'raised to the power of ' ,power, 'is equal to : ',ans)
